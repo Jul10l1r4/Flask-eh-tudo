1b3da03c69660078ab397a373db0ab68dc920ae3
jul10l1r4 <jul10l1r4@ufrn.edu.br>
2018-08-03 22:06:33 -0300

    Correria mdeus :computer:

---
+++ README.md
@@ -0,0 +1,2 @@
+# Palestra de: Conhecendo O MAGNIFICO FLAAAASKKKKKK!!!!!!!
+Aqui estarão os arquivos usados na `tentativa de live-code`
---
+++ erros-no-flask/main.py
@@ -0,0 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/grupy")
+def Wéu-come:
+	return "porra tu acertou o bagulho,djskfkdj"
